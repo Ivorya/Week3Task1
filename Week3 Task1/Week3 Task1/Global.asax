1cc4302a78144ba42c078915b5481997314af8f2
Ivorya <87962878+Ivorya@users.noreply.github.com>
2021-08-16 13:07:46 +1000

    Week3Task1
    Week3 Task1

---
+++ Week3 Task1/Week3 Task1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week3_Task1.MvcApplication" Language="C#" %>
---
+++ Week3 Task1/Week3 Task1/Models/Hello/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week3_Task1.Models.Hello
+{
+    public class Hello
+    {
+        public String GetHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
